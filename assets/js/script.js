608e4c7955b14c6d638a404e87335e8b8864a571
pablonunez17 <pablonunez17@gmail.com>
2023-10-16 00:06:53 -0300

    actualizando pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
 console.log("HOLA MUNDO")
+
+var nombre = "Pablo";
+console.log(typeof(nombre))
+
+let persona = {nombre: "Pablo", edad:42, hijos: false, sueldo: 32541.25,}
+console.log(typeof persona)
+console.log(persona)
